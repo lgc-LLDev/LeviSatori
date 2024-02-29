19ed65077bba26175288437849d6610aecd59dec
student_2333 <lgc2333@126.com>
2024-03-01 05:40:22 +0800

    up

--- packages/levi-satori/rollup.config.mjs
+++ packages/levi-satori/rollup.config.mjs
@@ -1,17 +1,17 @@
-import commonjs from '@rollup/plugin-commonjs';
-import json from '@rollup/plugin-json';
-import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from "@rollup/plugin-commonjs";
+import json from "@rollup/plugin-json";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
 // import terser from '@rollup/plugin-terser';
-import typescript from '@rollup/plugin-typescript';
+import typescript from "@rollup/plugin-typescript";
 
 export default [
   {
-    input: './src/index.ts',
+    input: "./src/index.ts",
     output: [
       {
-        name: 'NBSPlayer',
-        file: './dist/NBSPlayer.js',
-        format: 'cjs',
+        name: "LeviSatori",
+        file: "./dist/LeviSatori.js",
+        format: "iife",
       },
     ],
     plugins: [
